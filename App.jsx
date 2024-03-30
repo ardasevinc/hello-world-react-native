b5f7be98b6e0a6085b5e4c076f6dc408b4946bd2
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-03-30 23:11:33 +0300

    feat: app

--- App.jsx
+++ App.jsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import tw from './tailwind';
+import earthChan from './assets/icon.png';
 
 export default function App() {
   return (
-    <View style={tw`bg-[#00223a] flex flex-col justify-center items-center`}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={tw`bg-[#00223a] flex flex-col justify-center items-center h-full gap-y-8`}
+    >
+      <Text style={tw`text-2xl text-white max-w-[300px]`}>Hello World!</Text>
+      <TouchableOpacity style={tw`h-[400px] w-[400px]`}>
+        <Image style={tw`h-[400px] w-[400px]`} source={earthChan} />
+      </TouchableOpacity>
       <StatusBar style='auto' />
     </View>
   );
